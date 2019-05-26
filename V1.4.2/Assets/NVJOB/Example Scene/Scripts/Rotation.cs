ecd7eee0922dc6c2d702d304a5bff0cde36ce32d
#NVJOB <reset86@gmail.com>
2019-05-26 20:25:16 +0300

    V1.4.2

---
+++ V1.4.2/Assets/NVJOB/Example Scene/Scripts/Rotation.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+
+public class Rotation : MonoBehaviour
+{
+    public float rot = 1;
+
+    void Update()
+    {
+        transform.Rotate(Vector3.up, rot * Time.deltaTime);
+    }
+}
